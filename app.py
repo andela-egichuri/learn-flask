2eca10fb5790078921516920a279b27295803f1d
Eric G <eric.gichuri@andela.com>
2015-11-20 14:38:31 +0300

    Add index template

--- app.py
+++ app.py
@@ -1,18 +1,30 @@
-from flask import Flask
-from flask.ext.sqlalchemy import SQLAlchemy
 import os
+import requests
+from flask import Flask, render_template, request
+from flask.ext.sqlalchemy import SQLAlchemy
+
 
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 db = SQLAlchemy(app)
 
-from models import Result
 
-
-@app.route('/')
-def hello():
-	return "Hello World!"
+@app.route('/', methods=['GET', 'POST'])
+def index():
+	errors = []
+	results = {}
+	if request.method == "POST":
+		# get url that the user has entered
+		try:
+			url = request.form['url']
+			r = requests.get(url)
+			results = {"text" : r.text}
+		except:
+			errors.append(
+				"Unable to get URL. Please make sure it's valid and try again."
+			)
+	return render_template('index.html', errors=errors, results=results)
 
 
 @app.route('/<name>')
